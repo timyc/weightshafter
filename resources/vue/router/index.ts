7e2cd1ed89890cb6456c19588bdcf72a599c3b19
cs125 <cs125@delta.games>
2023-01-25 16:34:25 -0500

    Initialize Laravel with Vue

---
+++ resources/vue/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    /*{
+      path: "/other",
+      name: "other",
+      component: () => import("../views/OtherView.vue"),
+    },*/
+  ],
+});
+
+export default router;
