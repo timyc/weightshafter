3ecafbee04c5bb5236aa2c9e03d8f9e0acd0676b
Guanchen Wu <guanchw@uci.edu>
2023-02-23 04:32:56 -0500

    Resolved Issue - logic in HomeView

---
+++ resources/vue/components/home/UserInput.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+</script>
+
+
+<script lang="ts">
+export default{
+    data() 
+    {
+        return {
+        ht:0,
+        wt:0,
+        };
+    },
+}
+
+function getBMI(ht: number, wt: number){
+    var temp = ht / 100;
+    return wt / (temp*temp);
+}
+
+function assessBMI(bmi: number){
+    if (bmi < 18.5)
+    {
+        return "Underweight";
+    }
+    else if (bmi < 25)
+    {
+        return "Healthy";
+    }
+    else if (bmi < 30)
+    {
+        return "Overweight";
+    }
+    else
+    {
+        return "Obese";
+    }
+}
+
+</script>
+
+<template>
+    <div class="user-input-container">
+        <p>Enter your height(cm): <input v-model="ht" /></p>
+        <p>Enter your weight(kg): <input v-model="wt" /></p>
+        <p>Your BMI is: {{getBMI(ht, wt)}}</p>
+        <p>You are considered to be: {{assessBMI(getBMI(ht, wt))}}</p>
+        <button @click="$router.push({ name: 'foods' });" >Get Food Recommendations</button>
+    </div>
+</template>
--- resources/vue/views/HomeView.vue
+++ resources/vue/views/HomeView.vue
@@ -5,6 +5,7 @@ import Navbar from '@/components/Navbar.vue';
 import Tabbar from '@/components/Tabbar.vue';
 import Slider from '@/components/home/Slider.vue';
 import DailyHealth from '@/components/home/DailyHealth.vue';
+import UserInput from '@/components/home/UserInput.vue';
 import { useUserStore } from '@/stores/user';
 const userStore = useUserStore();
 const router = useRouter();
@@ -15,42 +16,6 @@ onMounted(() => {
 });
 </script>
 
-<!-- whoever wrote this, put this in another component because views should not have logic
-<script lang="ts">
-export default{
-  data() 
-  {
-    return {
-      ht:0,
-      wt:0,
-    };
-  },
-}
-
-function getBMI(ht, wt){
-    var temp = ht / 100;
-    return wt / (temp*temp);
-}
-
-function assessBMI(bmi){
-    if (bmi < 18.5)
-    {
-        return "Underweight";
-    }
-    else if (bmi < 25)
-    {
-        return "Healthy";
-    }
-    else if (bmi < 30)
-    {
-        return "Overweight";
-    }
-    else
-    {
-        return "Obese";
-    }
-}
-</script>-->
 
 <template>
     <div class="app-container with-navbar">
@@ -59,16 +24,9 @@ function assessBMI(bmi){
             <Slider />
             <DailyHealth />
             <Tabbar />
+            <UserInput />
         </div>
     </div>
-
-    <!--<div>
-        <p>Enter your height(cm): <input v-model="ht" /></p>
-        <p>Enter your weight(kg): <input v-model="wt" /></p>
-        <p>Your BMI is: {{getBMI(ht, wt)}}</p>
-        <p>You are considered to be: {{assessBMI(getBMI(ht, wt))}}</p>
-        <button @click="$router.push({ name: 'foods' });" >Get Food Recommendations</button>
-    </div>-->
 
 </template>
 
